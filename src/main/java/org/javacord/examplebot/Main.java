7cf907b65bf19479fb6a46f63daa92eeb070a4c1
sniperwolf <sniperwolf@Wolf>
2019-05-01 03:26:59 +0200

    Kommandozeilenoptionen optimiert

--- src/main/java/org/javacord/examplebot/Main.java
+++ src/main/java/org/javacord/examplebot/Main.java
@@ -18,16 +18,18 @@ public class Main {
      * @param args The arguments for the program. The first element should be the bot's token.
      */
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.err.println("Please provide a valid token as the first argument!");
-            return;
-        }
 
+    	if(!(args.length==2 && "-t".equals(args[0]) && args[1].length()>0))
+    	{
+    		System.err.println("Parameter: -t [token]");
+    		System.exit(1);
+    	}    
+    	
         // Enable debugging, if no slf4j logger was found
         FallbackLoggerConfiguration.setDebug(true);
 
         // The token is the first argument of the program
-        String token = args[0];
+        String token = args[1];
 
         // We login blocking, just because it is simpler and doesn't matter here
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
